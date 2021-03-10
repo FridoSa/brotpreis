7c7857752340b2ced1566bc0bfd382ba3e399638
Fridolin Sack <fridolin.sack@web.de>
2021-03-10 16:56:11 +0100

    interactive mode - select action

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,25 @@ module Main where
 
 import Lib
 
+dispatch :: [(Char, IO ())]
+dispatch = [ ('c', calculate)
+           , ('C', calculate)
+           , ('e', edit)
+           , ('E', edit)
+           ]
+
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "<C>alculate a price or <e>dit ingridients."
+    char <- getChar
+    getAction $ lookup char dispatch
+
+getAction :: Maybe (IO ()) -> IO ()
+getAction (Just action) = action
+getAction Nothing = putStrLn "Illegal character given."
+
+calculate :: IO ()
+calculate = putStrLn "calculate"
+
+edit :: IO ()
+edit = putStrLn "edit"
