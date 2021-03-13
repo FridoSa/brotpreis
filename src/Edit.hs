54b0bdcdb008737d8db1af35245f8f0fbca46626
Fridolin Sack <fridolin.sack@web.de>
2021-03-13 23:17:36 +0100

    WIP: remove items; error handling missing

--- src/Edit.hs
+++ src/Edit.hs
@@ -5,6 +5,8 @@ module Edit
 import Lib
 import System.IO
 import System.IO.Error
+import System.Directory
+import Data.List
 
 dispatch :: [(String, [String] -> IO ())]
 dispatch = [ ("add", add)
@@ -13,23 +15,27 @@ dispatch = [ ("add", add)
             ,("ret", (\_ -> return ()))
            ]
 
-handelFileNotExists :: IOError -> IO ()
-handelFileNotExists e 
+handleFileNotExists :: IOError -> IO ()
+handleFileNotExists e 
     | isDoesNotExistError e = putStrLn "No ingredients saved."
     | otherwise = ioError e
 
 getAction :: Maybe ([String] -> IO ()) -> [String] -> IO ()
 getAction (Just action) arguments = action arguments
-getAction Nothing _ = putStrLn "Illegal command."
+getAction Nothing _ = do 
+    putStrLn "Illegal command."
+    edit
 
 edit :: IO ()
 edit = do
-    showIngredients `catchIOError` handelFileNotExists
+    showIngredients `catchIOError` handleFileNotExists
     putStrLn "Type add followed by a new entry to add an item. Attributes must be whitespace-seperated."
     putStrLn "Type ret to return."
     command <- getLine
     case words command of (action : arguments) -> getAction (lookup action dispatch) arguments
-                          [] -> putStrLn "No command given."
+                          [] -> do 
+                              putStrLn "No command given."
+                              edit
     
 showIngredients :: IO ()
 showIngredients = do
@@ -38,12 +44,32 @@ showIngredients = do
     putStrLn "Type ed followed by a number and a changed entry to edit an item. Attributes must be whitespace-seperated."
 
 add :: [String] -> IO ()
+add [] = do
+    putStrLn "No item given."
+    edit
 add i = do
     appendFile "ingredients.txt" (unwords i ++ "\n")
     edit
 
 remove :: [String] -> IO ()
-remove a = do
+remove [] = do
+    putStrLn "No number given."
+    edit
+remove (x:_) = do
+    let number = read x
+        fileName = "ingredients.txt"
+    handle <- openFile fileName ReadMode  
+    (tempName, tempHandle) <- openTempFile "." "temp"  
+    contents <- hGetContents handle  
+    let items = lines contents  
+        newItems = if number < length items
+                     then delete (items !! number) items
+                     else items
+    hPutStr tempHandle $ unlines newItems  
+    hClose handle  
+    hClose tempHandle  
+    removeFile fileName  
+    renameFile tempName fileName
     edit
 
 editEntry :: [String] -> IO ()
